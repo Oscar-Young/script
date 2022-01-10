bc0d0e61a528f561c49708661ac1ee2a6e1515cc
Oscar.Young <Oscar.Young@sparktech.io>
2022-01-10 13:34:28 +0800

    iotedge

---
+++ script/iotedge.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+source /etc/os-release
+
+if [ "$NAME" != 'Ubuntu' ]
+then
+  echo "$NAME:$VERSION is not support"
+  exit 1
+fi
+
+if [ "$VERSION" == '18.04' ] && [ "$VERSION" == '20.04' ]
+then
+  echo "$NAME:$VERSION is not support"
+  exit 1
+fi
+
+
+
+curl https://packages.microsoft.com/config/ubuntu/18.04/multiarch/prod.list > ./microsoft-prod.list && \
+sudo cp ./microsoft-prod.list /etc/apt/sources.list.d/ && \
+curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg && \
+sudo cp ./microsoft.gpg /etc/apt/trusted.gpg.d/ && \
+sudo apt update && \ sudo apt -y install moby-engine && sudo apt install -y aziot-edge
