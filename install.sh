b345dd70ffe9f8662b8b18ef8a5c0e3156a0f15b
Oscar-Young <oscar08824@gmail.com>
2022-07-11 10:53:40 +0800

    Add

--- install.sh
+++ install.sh
@@ -17,4 +17,7 @@ case "${1}" in
     -get)
         curl "https://raw.githubusercontent.com/Oscar-Young/script/main/script/${2}.sh" > "${2}".sh
     ;;
+    *)
+        echo "Argument : ${1}" not found.
+    ;;
 esac
